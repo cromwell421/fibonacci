f4338a5dfa528e4f29b872bdf15484dacd45d971
cromwell421 <32655686+cromwell421@users.noreply.github.com>
2018-02-06 21:12:12 -0500

    Update fibonacci.py
    Added user input and more error checks

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,26 @@
 def fib(n):
   list = [0,1]
   count = 0
-  if type(n) is int:
-    for i in range(n-1):
+  
+  if type(n) is not int:
+    print("Error: arg not int")
+  
+  elif n < 1:
+    print("That number is less than 1!!!")
+  
+  elif n == 1:
+    print list[0]
+
+  else:
+    for i in range(n-2):
       count = list[-2] + list[-1]
       list.append(count)
     print list
-  else:
-    print("Error: arg not int")
 
 
 def main():
-  print 'test'
-  fib(25)
+  choice = (input("Pick an integer 1-100: ")
+  fib(choice)
 
 if __name__ == '__main__':
   main()
